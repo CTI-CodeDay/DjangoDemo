4834f4311fd97c2cd14d2ae1319b58ecbccf9690
Romano Edwin Ramirez <70995300+rramirez77@users.noreply.github.com>
2022-06-14 02:57:41 +0000

    Functional Page view

--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import homeView
 urlpatterns = [
+    path('', homeView, name='home'),
     path('admin/', admin.site.urls),
 ]
