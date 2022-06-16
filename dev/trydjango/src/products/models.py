571023c329cd78419f30c31e915c280b9594d3d6
thinhngocnguyen <59405079+thinh241819@users.noreply.github.com>
2022-06-16 13:25:10 +0000

    changed fields in products model, had to deleted database

--- dev/trydjango/src/products/models.py
+++ dev/trydjango/src/products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 # new Product class inherite Model class from basic models class in django
 class Product(models.Model):
-  title       = models.TextField()
-  description = models.TextField()
-  price       = models.TextField()
+  title       = models.CharField(max_length=120)
+  description = models.TextField(blank=True, null=True)
+  price       = models.DecimalField(decimal_places=2, max_digits=1000)
+  summary     = models.TextField(default='this is cool!')
