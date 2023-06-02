a34421faeeee5e2e852156877aa8758d6c2cc91f
Rahul <122141535+rahulio96@users.noreply.github.com>
2023-06-02 21:59:03 +0000

    Modified product model

--- Dev/trydjango/src/products/models.py
+++ Dev/trydjango/src/products/models.py
@@ -6,6 +6,10 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary = models.TextField()
-    featured - models.BooleanField()
+    summary = models.TextField(blank=True, null=False) # Allows us to leave summary empty (not requried)
+
+    # null has to do with db while blank has to do with a required field or not
+
+
+    featured = models.BooleanField() # null=True, default=True
     # default = 'this is cool!'
