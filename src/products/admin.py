4c8b3db0ad76991f0368687b0cb3e51d7fc215b6
Kevin Beltran <122430858+KevinBeltran23@users.noreply.github.com>
2024-11-01 04:36:54 +0000

    other

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Product2
+from .models import Product
 
-admin.site.register(Product2)
+admin.site.register(Product)
--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Product2(models.Model):
+class Product(models.Model):
     title       = models.CharField(max_length=122) # max length is required
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
