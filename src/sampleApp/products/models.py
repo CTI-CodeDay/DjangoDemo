be1888d608c92cfd7f588d9a4c3921194d1d7f7a
Alyosha-ctrl <alexeibere@gmail.com>
2025-01-04 08:50:55 +0000

    Final commit for the requirements of the assignment

--- src/sampleApp/products/models.py
+++ src/sampleApp/products/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     description = models.TextField(blank = True, null = True)
     price = models.DecimalField(decimal_places =2, max_digits = 1000)
     summary = models.TextField(default = 'This is Cool!')
+    featured = models.BooleanField(default = False)
